7ea74f7c20fd53ed40ac577b2fa2c10ea354ac97
Mohd Mustafa Ahmed <mustafamohdvvip@gmail.com>
2025-01-18 19:50:03 +0530

    Add files via upload

---
+++ string1.js
@@ -0,0 +1,8 @@
+let dd="prem"
+let friend="chNnd"
+console.log(friend.toUpperCase())
+console.log(friend.toLowerCase())
+console.log(dd.concat(" he is a friend of ",friend))
+//console.log(name.toUppercase
+let his="the way he talk i like thw most because of the attractive nesss in the Face "
+console.log(his.replaceAll("the","$"))
